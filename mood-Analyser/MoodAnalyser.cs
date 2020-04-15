cf02e793ad75c524eeb0694a7872154b9dd5d083
pranaliandre <pranaliandre06@gmail.com>
2020-04-15 13:18:34 -0500

    Added: Ability to analyse check happy or sad mood

--- mood-Analyser/MoodAnalyser.cs
+++ mood-Analyser/MoodAnalyser.cs
@@ -2,11 +2,42 @@
 
 namespace mood_Analyser
 {
-    class MoodAnalyser
+    public class MoodAnalyser
     {
+        private string message;
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public MoodAnalyser() { }
+        /// <summary>
+        /// Parameterized constructor
+        /// </summary>
+        /// <param name="message"> message </param>
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        /// <summary>
+        /// analyze mood method
+        /// </summary>
+        /// <returns> happy or sad depending upon message </returns>
+        public string AnalyseMood()
+        {
+            if (message.Contains("sad"))
+                return "sad";
+            else
+                return "happy";
+        }
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("!!!Welcome To MoodAnalyser!!!");
+            Console.WriteLine("Welcome to Mood Analyser");
         }
+
     }
 }
