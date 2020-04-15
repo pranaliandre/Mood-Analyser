06c7c087bc07ab4f7729b0e604f2fdb17f849015
pranaliandre <pranaliandre06@gmail.com>
2020-04-15 13:32:51 -0500

    Added: Test case 2 given any mood return should happy mood

--- moodAnalyserTest/MoodAnalyserTest.cs
+++ moodAnalyserTest/MoodAnalyserTest.cs
@@ -9,13 +9,25 @@ namespace moodAnalyserTest
         {
         }
         /// <summary>
-        /// Analyse sad mood method 
+        /// Analyse sad mood method test
         /// </summary>
         [Test]
         public void GivenSadMood_WhenProper_ShouldReturnSad()
         {
             string expected = "sad";
             string message = "i am in sad mood";
+            MoodAnalyser object_Mood = new MoodAnalyser(message);
+            string mood = object_Mood.AnalyseMood();
+            Assert.AreEqual(expected, mood);
+        }
+        /// <summary>
+        /// Analyse Happy method test
+        /// </summary>
+        [Test]
+        public void GivenHappyMood_WhenProper_ShouldReturnHappy()
+        {
+            string expected = "happy";
+            string message = "i am in any mood";
             MoodAnalyser object_Mood = new MoodAnalyser(message);
             string mood = object_Mood.AnalyseMood();
             Assert.AreEqual(expected, mood);
